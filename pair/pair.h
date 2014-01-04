b50d825c82e8342f3cd72cba8082f451a0a72d2b
basch3034 <schummerb@gmail.com>
2014-01-04 19:23:01 +0100

    - download termine seulement pour 1 partition - connexion initiale au serveur - ecoute des requetes de telechargement de partition pour un pair - debut upload

---
+++ pair/pair.h
@@ -0,0 +1,16 @@
+typedef struct Pair Pair;
+struct Pair {
+    char* data;
+    Pair *suiv;
+};
+
+typedef struct ListePair ListePair;
+struct ListePair {
+    Pair *prem;
+};
+
+typedef struct ParamPartition ParamPartition;
+struct ParamPartition {
+    struct sockaddr_in add;
+    char* cleFichier;
+};
